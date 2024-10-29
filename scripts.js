d51a344f893427d2f8568c949f0e3acf798ba23c
clestialMusepi <clestialmusepi@gmail.com>
2024-10-29 18:41:14 +0100

    violence

--- scripts.js
+++ scripts.js
@@ -376,6 +376,7 @@ const motsClasses = {
 "intéractions":"couleur3",
 "chromosomes":"couleur3",
 "Nous":"couleur3",
+"masse":"couleur3",
 "groupe":"couleur3",
 "sommes":"couleur3",
 "même":"couleur3",
