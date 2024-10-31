5f58e2b4c3e8b6fb48f694ace2ab81623cddfa7a
clestialMusepi <clestialmusepi@gmail.com>
2024-10-31 01:49:18 +0100

    violence

--- js/abc.js
+++ js/abc.js
@@ -317,6 +317,7 @@ const motsClasses = {
 "future":"couleur2",
 "conjonction":"couleur2",
 // C
+"naviguer":"couleur3",
 "problèmes":"couleur3",
 "compréhension":"couleur3",
 // "organisé":"couleur3",
