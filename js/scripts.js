3b6ddc2d3ad0bdbf3a97f48665084a46d6f25ece
clestialMusepi <clestialmusepi@gmail.com>
2024-11-26 12:04:57 +0100

    violence

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ const Lien = {
     pageAcceuil: '<a href="index.html"><li id="lienPageAcceuilCouleur">Acceuil</li></a>',
     pageMonLore: '<a href="html/pageMusique.html"><li id="lienPageMusiqueCouleur">Musique</li></a>',
     pageMusique: '<a href="html/pageMonLore.html"><li id="lienPageMonLoreCouleur">Mon lore</li></a>',
-    pageNews: '<a href="html/pageNews.html"><li id="lienPageNews">News !</li></a>',
-    pageReseaux: '<a href="html/pageReseaux.html"><li id="lienPageReseaux">Réseaux</li></a>',
-    pageToDoList: '<a href="html/pageToDoList.html"><li id="lienPageToDoList">To Do List</li></a>',
+    pageNews: '<a href="html/pageNews.html"><li id="lienPageNewsCouleur">News !</li></a>',
+    pageReseaux: '<a href="html/pageReseaux.html"><li id="lienPageReseauxCouleur">Réseaux</li></a>',
+    pageToDoList: '<a href="html/pageToDoList.html"><li id="lienPageToDoListCouleur">To Do List</li></a>',
 }
 const listLiens = 
 `
--- js/scripts2.js
+++ js/scripts2.js
@@ -3,8 +3,8 @@ const Lien = {
     pageMonLore: '<a href="../html/pageMusique.html"><li id="lienPageMusiqueCouleur">Musique</li></a>',
     pageMusique: '<a href="../html/pageMonLore.html"><li id="lienPageMonLoreCouleur">Mon lore</li></a>',
     pageNews: '<a href="../html/pageNews.html"><li id="lienPageNews">News !</li></a>',
-    pageReseaux: '<a href="../html/pageReseaux.html"><li id="lienPageReseaux">Réseaux</li></a>',
-    pageToDoList: '<a href="../html/pageToDoList.html"><li id="lienPageToDoList">To Do List</li></a>',
+    pageReseaux: '<a href="../html/pageReseaux.html"><li id="lienPageReseauxCouleur">Réseaux</li></a>',
+    pageToDoList: '<a href="../html/pageToDoList.html"><li id="lienPageToDoListCouleur">To Do List</li></a>',
 }
 const listLiens = 
 `
