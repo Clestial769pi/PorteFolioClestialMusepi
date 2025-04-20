1501d8a94adba1fdd5699a571474b3878fc7ace4
clestialMusepi <clestialmusepi@gmail.com>
2025-04-20 12:09:28 +0200

    Clestial Musepi 2025

--- script.js
+++ script.js
@@ -48,6 +48,8 @@ const motsClasses2 = {
     "Le nombre de la bête":"couleur3",
     "Vie":"couleur1",
     "Mort":"couleur2",
+    "Début":"couleur1",
+    "Fin":"couleur2",
 };
         function appliquerClassesTexte(element) {
             let ABC = element.innerHTML;
