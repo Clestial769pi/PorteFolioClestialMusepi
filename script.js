26dbcef94729e09b5a7c17803ba10ed783c64aae
clestialMusepi <clestialmusepi@gmail.com>
2025-04-20 12:41:07 +0200

    Clestial Musepi 2025

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ const motsClasses2 = {
     "Fin":"couleur2",
     "entrechoquer":"couleur3",
     "corruption":"couleur3",
+    "Terre":"couleur3",
 };
         function appliquerClassesTexte(element) {
             let ABC = element.innerHTML;
